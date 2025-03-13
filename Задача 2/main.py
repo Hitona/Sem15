5fa0fda6a72a2732fd59c451849b78c4492174b4
Diana <70949386+Hitona@users.noreply.github.com>
2025-03-13 09:44:34 +0300

    Add files via upload

---
+++ Задача 2/main.py
@@ -0,0 +1,8 @@
+def sum_range(M, N):
+    return sum(range(M, N + 1))
+
+# Пример использования
+M = 1
+N = 15
+result = sum_range(M, N)
+print(f"Сумма элементов в промежутке от {M} до {N}: {result}")
